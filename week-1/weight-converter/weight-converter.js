5dbccdd2ff3be723a20980623d3f7df337cb9990
Nadia <nadiagainer@gmail.com>
2024-03-24 21:15:56 -0500

    update

---
+++ week-1/weight-converter/weight-converter.js
@@ -0,0 +1,16 @@
+/**
+ * Author:Nadia Gainer
+ * Date: 03/22/24
+ * File Name: 
+ * Description:
+*/
+
+"use strict";
+
+// TODO: Implement the weight conversion logic here
+
+function weightConverter(valNum) {
+  document.getElementById("outputGrams").innerHTML = valNum / 0.0022046;
+  }
+
+  
