218f43a5fab4b016f2bc34d262b7ace9e76a71a4
Nadia <nadiagainer@gmail.com>
2024-04-21 21:32:36 -0500

    update

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,20 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:Nadia Gainer
+ * Date:04/21/24
+ * File Name: pie
+ * Description:pie
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
   // Your code here
+const essentialingredients=["flour","sugar","butter"]
+  for (const essentialingredient of essentialingredients){
+  if (!ingredients.includes(essentialingredient)){
+  console.warn(`You Are Missing ${essentialingredient}`)
+  process.exit(1) 
 }
-
+}
+  return `Your ${pieType} Pie Was Successfully Baked`
+}
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,8 +1,8 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Nadia Gainer
+ * Date:04/ 21/24
+ * File Name:pie.specs.js
+ * Description:pie-baker 
  */
 
 "use strict";
@@ -10,3 +10,32 @@
 const { bakePie } = require("../src/pie");
 
 // Your tests here
+//
+describe("bakePie test", ()=>{
+it("test1",()=>{
+
+
+			const ingredients= ["flour", "sugar", "butter", "egg"]
+	   const pie = bakePie("Pumpkin",ingredients)
+		expect(pie).toEqual("Your Pumpkin Pie Was Successfully Baked")
+
+}) 
+it("test2",()=>{
+		console.warn = jest.fn()
+		process.exit = jest.fn()
+		const ingredients= ["flour", "sugar", "egg"]
+	bakePie("Pumpkin",ingredients)
+	expect(console.warn).toHaveBeenCalledWith(`You Are Missing butter`)
+	expect(process.exit).toHaveBeenCalledWith(1)
+	}) 
+
+	it("test3",()=>{
+		console.warn = jest.fn()
+		process.exit = jest.fn()
+		const ingredients= ["flour", "egg"]
+	bakePie("Pumpkin",ingredients)
+	expect(console.warn).toHaveBeenCalledWith(`You Are Missing butter`)
+	expect(console.warn).toHaveBeenCalledWith(`You Are Missing sugar`)
+	expect(process.exit).toHaveBeenCalledWith(1)
+	})
+}) 
