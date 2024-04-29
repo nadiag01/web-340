6d8d7d9d0901fb27d1b76f35e44573f0b923c11c
Nadia <nadiagainer@gmail.com>
2024-04-28 20:08:11 -0500

    update

--- week-6/fantasy-game-character-creation/src/server.js
+++ week-6/fantasy-game-character-creation/src/server.js
@@ -4,11 +4,49 @@ const url = require('url');
 // TODO: Implement your server here
 
 const server = http.createServer((req, res) => {
+ const urlPath=url.parse(req.url,true)
   // TODO: Implement your routes here
+  if(req.method==="POST" && urlPath.pathname==="/create"){
+create(req,res)
+  }
+  else if(req.method ==="POST" && urlPath.pathname==="/confirm"){
+    confirm(req,res)
+  }
+  else if (req.method ==="GET" && urlPath.pathname==="/view"){
+view(req,res)
+  }
 });
 
+let character= {}
+const create=(req,res) =>{
+let body=""
+req.on("data", (segment)=>{
+  body+=segment.toString()
+})
+req.on("end", ()=>{
+ 
+  const data= JSON.parse(body)
+  character= {
+    class: data.class, gender: data.gender, funFact: data.funFact 
+  }
+  console.log(character)
+  res.end(JSON.stringify({message:"character created"}))
+})
+}
+const confirm = (req,res) =>{
+  if (character)
+  res.end(JSON.stringify({message:"character exists"}))
+else 
+res.end(JSON.stringify({message:"character does not exist"}))
+}
+const view=(req,res) => {
+  if (character)
+  res.end(JSON.stringify({character: character}))
+else 
+res.end(JSON.stringify({message:"character does not exist"}))
+}
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
-module.exports = server;
+module.exports = {server,create,confirm,view};
--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,41 @@
 const http = require('http');
-const server = require('../src/server');
+const {server,create,confirm,view} = require('../src/server');
 
 // TODO: Implement your tests here
+describe("routetests",() =>{
+    it("create a character",()=> {
+const req= {
+    on: jest.fn((event,callback)=> {
+        if (event==="data")
+        callback(JSON.stringify({class: "Warrior", gender: "female", funFact:"test"}))
+    if (event==="end")
+    callback()
+    })
+}
+const res={
+    end:jest.fn()
+}
+create(req,res)
+expect(res.end).toHaveBeenCalledWith(JSON.stringify({message: "character created"}))
+    })
+    
+    it("confirm a character",()=> {
+        const req= {
+        }
+        const res={
+            end:jest.fn()
+        } 
+        confirm(req,res)
+        expect(res.end).toHaveBeenCalledWith(JSON.stringify({message: "character exists"}))
+    })
+
+    it("view a character",()=> {
+        const req= {
+        }
+        const res={
+            end:jest.fn()
+        } 
+        view(req,res)
+        expect(res.end).toHaveBeenCalledWith(JSON.stringify({character: {class: "Warrior", gender: "female", funFact:"test"}}))
+    })
+})
