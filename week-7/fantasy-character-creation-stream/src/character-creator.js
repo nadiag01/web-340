bdc395e32f07a81d2abbe12bb002082fcfbdd91f
Nadia <nadiagainer@gmail.com>
2024-05-05 11:23:14 -0500

    updates

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -4,14 +4,31 @@ class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
     // TODO: Initialize your class here
+    this.character={
+      class: "", gender: "", funfact: ""
+    }
   }
 
   _write(chunk, encoding, callback) {
     // TODO: Implement your _write method here
-  }
+    const data=chunk.toString().trim().split(",")
+ if (data.length===3){
+  const characterclass=data[0].trim()
+const charactergender=data[1].trim()
+const characterfunfact=data[2].trim()
+this.character.class=characterclass
+this.character.gender=charactergender
+this.character.funfact=characterfunfact
+callback()
+ }else{
+  this.emit("error",new Error("invalid data"))
+ } }
 
   _read(size) {
     // TODO: Implement your _read method here
+    const format=`class: ${this.character.class}, gender: ${this.character.gender}, funfact: ${this.character.funfact}`
+    this.push(format)
+    this.push(null)
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -9,13 +9,37 @@ describe('CharacterCreator', () => {
 
   test("should process data correctly when written to", (done) => {
     // TODO: Write your test here
+    const character1="mage,male, has a wand"
+    characterCreator.on("data",()=>{
+      expect(characterCreator.character.class).toBe("mage")
+      expect(characterCreator.character.gender).toBe("male")
+      expect(characterCreator.character.funfact).toBe("has a wand")
+      done()
+    })
+    characterCreator.write(character1)
+    characterCreator.end()
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
     // TODO: Write your test here
+    characterCreator.on("error",(error)=>{
+      expect(error.message).toBe("invalid data")
+     
+      done()
+    })
+    characterCreator.write("")
+    characterCreator.end()
   });
 
   test("should transform data correctly when written to", (done) => {
     // TODO: Write your test here
+    const character2="warrior,female,has a shield"
+    characterCreator.on("data",(data)=>{
+      expect(data.toString()).toBe("class: warrior, gender: female, funfact: has a shield")
+     
+      done()
+    })
+    characterCreator.write(character2)
+    characterCreator.end()
   });
 });
